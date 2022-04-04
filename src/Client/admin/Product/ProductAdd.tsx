4783a4446116c311b73aaf4e76b69ad914862201
Le Vu <levu08072002@gmail.com>
2022-04-04 16:05:29 +0700

    update product img into cloudinary

--- src/Client/admin/Product/ProductAdd.tsx
+++ src/Client/admin/Product/ProductAdd.tsx
@@ -1,21 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { Form, Input, Button, InputNumber, Switch, Select } from "antd";
+import { Form, Input, Button, InputNumber, Switch, Select, Progress, notification } from "antd";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { ProductType } from "../../Types/product";
-import { Alert } from "antd";
-import { Option } from "antd/lib/mentions";
 import {Categorylist} from '../../Api/category'
+import { Upload } from 'antd';
+import ImgCrop from 'antd-img-crop';
+import axios from "axios";
 type ProductAddProps = {
   onAdd: (product: ProductType) => void;
 };
 
 const ProductAdd = (props: ProductAddProps) => {
-  const [category, setCategory] = useState([])
+const [url,setUrl] = useState([])
+const [category, setCategory] = useState([])
+  const [defaultFileList, setDefaultFileList] = useState([]);
+  const uploadImage = async (options:any) => {
+    const { onSuccess, onError, file } = options;
+    const formData = new FormData();
+    
+    formData.append("file", file);
+    formData.append("upload_preset", "ypnhyinn");
+    try {
+      const res = await axios({
+        url: "https://api.cloudinary.com/v1_1/ecma/image/upload",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-formendcoded",
+        },
+        data: formData,
+      }
+      
+      );
+      onSuccess('Ok');
+      notification.success({
+       "message" : "Upload successfully !",
+       "description": "Image uploaded successfully to Cloudinary !!"
+      })
+      console.log("server res: ", res);
+      setUrl([...url,{imgname: res.data.original_filename,url : res.data.secure_url} ])
+    } catch (err) {
+      console.log("Error: ", err);
+      const error = new Error("Some error");
+      onError({ err });
+    }
+  };
+
+  const handleOnChange = ({ file, fileList, event }:any) => {
+    setDefaultFileList(fileList);
+    console.log(file);
+    
+  };
     const navigate = useNavigate();
   const onFinish = (values: ProductType) => {
-    console.log("Success:", values);
-    props.onAdd(values);
+    const newValues = {...values, image : url}
+    console.log("Success:", newValues);
+    props.onAdd(newValues);
     navigate("/admin/product");
   };
 
@@ -30,7 +70,6 @@ const ProductAdd = (props: ProductAddProps) => {
     };
     getCategory();
   },[])
-  console.log(category);
   
   return (
     <Form
@@ -39,7 +78,7 @@ const ProductAdd = (props: ProductAddProps) => {
       autoComplete="off"
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
-      initialValues={{ status : true }}
+      initialValues={{ status : true, }}
     >
       <Form.Item label="Product Name" name="name" rules={[{ required: true, message:"Please input product name"},{min: 5 , message : "Product name must be at least 5 characters"}]}>
         <Input />
@@ -58,8 +97,19 @@ const ProductAdd = (props: ProductAddProps) => {
       >
         <Input.TextArea />
       </Form.Item>
-      <Form.Item label="URL" name="image" rules={[{ required: true, message:"Please input image" }]}>
-        <Input placeholder="input placeholder" />
+      <Form.Item label="image" name = "image">
+<ImgCrop>
+<Upload
+        accept="image/*"
+        customRequest={uploadImage}
+        onChange={handleOnChange}
+        listType="picture-card"
+        defaultFileList={defaultFileList}
+        className="image-upload-grid"
+      >
+        {defaultFileList.length < 5  && '+ Upload'}
+      </Upload>
+</ImgCrop>
       </Form.Item>
       <Form.Item label="Select" name="category" >
         <Select>
