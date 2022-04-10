188c5c6e14e652b38acd2af8458746cffabe2fa1
Le Vu <levu08072002@gmail.com>
2022-04-11 03:52:59 +0700

    uploads api

--- src/Routes/product.js
+++ src/Routes/product.js
@@ -9,9 +9,10 @@ router.get('/products',get);
 router.post('/products/:userId',requiredSigin,isAuth,isAdmin,upload.single('image'),create);
 router.get('/product/:id',getOne);
 router.delete('/product/:id/:userId',requiredSigin,isAuth,isAdmin,remove);
-router.put('/product/:id/:userId',requiredSigin,isAuth,isAdmin,update);
+router.put('/product/:id/:userId',requiredSigin,isAuth,isAdmin,upload.single('image'),update);
 router.get('/products-latest',getLatest)
 router.get('/products/search',search)
 router.get('/products/filter')
-router.param('userId', userById)
+router.param('userId', userById)m
+
 export default router;
